02cab4673ff514c89ead8434b71bf5e926b67ae2
Tony Lin <tonylin0814@gmail.com>
2026-06-21 15:41:38 -0400

    Include Chromium files for Vercel PDF function

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ["@sparticuz/chromium"],
+    serverComponentsExternalPackages: [
+      "@sparticuz/chromium",
+      "puppeteer-core",
+    ],
+    outputFileTracingIncludes: {
+      "/api/records/receipts/[record_r_number]/download": [
+        "./node_modules/@sparticuz/chromium/bin/**/*",
+        "./node_modules/@sparticuz/chromium/build/**/*",
+      ],
+    },
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        "@sparticuz/chromium": "commonjs @sparticuz/chromium",
+        "puppeteer-core": "commonjs puppeteer-core",
+      });
+    }
+
+    return config;
   },
 };
 
